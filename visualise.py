c4f3caef6dff3df7b700e5c62ff8b11afcfbcfc8
ipeglin <philip@eglin.no>
2021-08-06 16:29:02 +0200

    Added more to the recursive function. Also added a limiter that determines how far down the recursion will go. ERROR! it appears to be a problem for the script to separate files and folders when it has recursed down a level. Therefore the folders are just listed as files. It also looks like the name of the new folder is displayed incorrectly

--- visualise.py
+++ visualise.py
@@ -1,14 +1,38 @@
 import os
+import sys
 from pathlib import Path
 
-current_path = os.getcwd()
-print(current_path)
+def list_content(path, level, limit):
+    if (level <= limit):
+        # Setting a prefix that indicates how deep the recursion has gone
+        prefix = ("=" * level) + ">"
+        print(f"{prefix} {os.path.realpath(path)}") # Print the current directory path
 
-def list_content(path):
-    print(f"=== Searching DIR: {os.path.realpath(path)} ===")
-    files = [os.path.realpath(file) for file in os.listdir(path) if not os.path.isdir(file)]
-    print("FILES IN DIRECTORY:", [Path(file).name for file in files], "\n") # Printing the files in the directory
+        # Listing all the files withing the current directory
+        files = [os.path.realpath(file) for file in os.listdir(path) if not os.path.isdir(file)]
+        [print(f"    - {(Path(file).name)}") for file in files] # Print the files in the directory
+        
+        print("\n") # Print new line before recursing to new sub-directory
+        
+        # Increment the level count to indicate how deep the recursion has gone
+        level += 1
+        # Run the recursive function for each of the sub-directory in the current folder
+        [list_content(os.path.realpath(DIR), level, limit) for DIR in os.listdir(path) if os.path.isdir(DIR)]
 
-    [list_content(os.path.realpath(DIR)) for DIR in os.listdir(path) if os.path.isdir(DIR)]
+def get_recursion_level():
+    if (get_sys_args()):
+        recursion_limit = int(get_sys_args()[0])
+    else:
+        recursion_limit = int(input("Recursion limit: "))
+    return recursion_limit
+    
 
-list_content(current_path)
+def get_sys_args():
+    try:
+        recursion_limit = sys.argv[1]
+        return [recursion_limit]
+    except:
+        return []
+
+# Run the recursive function in the current working directory
+list_content(os.getcwd(), 0, get_recursion_level())
