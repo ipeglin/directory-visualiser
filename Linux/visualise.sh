b6aa4f13837146865a65bb0a437d730b00106eef
ipeglin <philip@eglin.no>
2021-08-07 17:18:32 +0200

    Wrote the rest of the bash script code for the Linux visualiser. I still havent adde dcomments to the scriptW

--- Linux/visualise.sh
+++ Linux/visualise.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-clear
-
 FILE_PREFIX="└"
 INDENTATION_PREFIX="─"
 FOLDER_PREFIX=">"
@@ -9,30 +7,54 @@ FOLDER_PREFIX=">"
 # Recursive function for file traversing
 listDirectory() {
     CURRENT_PATH="$1"
+    BASENAME=$(basename $CURRENT_PATH)
     declare -i LEVEL=$2
     declare -i LIMIT=$3
-    FULL_FILE_PREFIX="TEST"
+    FULL_FILE_PREFIX=""
     
     if [ ${LIMIT} -lt 1 ]; then
-        LIMIT=1
+        LIMIT=$((1))
     fi
 
     if [ $LEVEL -lt $LIMIT ]; then
-        PREFIX_LENGTH=$(($LIMIT + 2))
+        PREFIX_LENGTH=$(($LEVEL + 2))
 
         FULL_FILE_PREFIX="$FILE_PREFIX$(seq -s $INDENTATION_PREFIX $PREFIX_LENGTH | tr -d '[:digit:]')"
+
+        declare -a CONTENT=($CURRENT_PATH/*)
+        declare -a FILES=()
+        declare -a FOLDERS=()
+
+        for item in ${CONTENT[@]}; do
+            local ITEMNAME=$(echo $item | sed "s/.*\///")
+            if [[ $ITEMNAME == *"."* ]]; then
+                FILES+=($item)
+            else
+                FOLDERS+=($item)
+            fi
+        done
+
+        LEVEL=$(($LEVEL + 1))
+        echo "$(seq -s $FOLDER_PREFIX $PREFIX_LENGTH | tr -d '[:digit:]') $BASENAME"
+        for file in ${FILES[@]}; do
+            echo "$FULL_FILE_PREFIX $(echo $file | sed 's/.*\///')"
+        done
+
+        for folder in ${FOLDERS[@]}; do
+            listDirectory $folder $LEVEL $LIMIT
+        done
     fi
 
-    echo "PATH: $CURRENT_PATH"
-    echo "LEVEL: $LEVEL"
-    echo "LIMIT: $LIMIT"
-    echo "PREFIX LENGTH: $PREFIX_LENGTH"
-    echo "FILE PREFIX: $FILE_PREFIX"
-    echo "INDENTATION PREFIX: $INDENTATION_PREFIX"
-    echo "FULL FILE PREFIX: $FULL_FILE_PREFIX"
-    echo "FOLDER PREFIX: $FOLDER_PREFIX"
-
-    read
+    # read
 }
 
-listDirectory $PWD 0 -5
+Main() {
+    { # Try
+        read -p "Recursion Limit: " RECURSION_LIMIT
+    } || { # Catch
+        declare -i RECURSION_LIMIT=1
+    }
+    listDirectory $PWD 0 $RECURSION_LIMIT
+}
+
+Main
